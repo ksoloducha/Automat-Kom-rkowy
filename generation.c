c445b324ff1c5f2c376908ce34cbebc7fc7e5f6e
kamx9 <61712110+kamx9@users.noreply.github.com>
2020-03-14 21:47:00 +0100

    modyfikacja warunków, uzupełnienie reguł, estetyka

--- generation.c
+++ generation.c
@@ -5,9 +5,9 @@
 estern int XMAX, YMAX;
 
 void move (bool **tab){
+  // tymczasowa tablica ze stanami początkowymi
   bool temp[XMAX][YMAX];
-
-  for (int i = 0; i < XMAX; i++ ){   // tymczasowa tablica ze stanem poczatkowym
+  for (int i = 0; i < XMAX; i++ ){
     for (int j = 0; j < YMAX; j++ ){
       temp[i][j] = tab[i][j];
     }
@@ -16,111 +16,127 @@ void move (bool **tab){
   int neighbors;
   for (int i = 0; i < XMAX; i++ ){
     for (int j = 0; j < YMAX; j++ ){
-      neighbors = 0;  // ilosc sasiadow
-
-      if (temp[i][j] == false){  // jesli martwa ma conajmniej 3
-                                 // zywych sasiadow staje sie zywa
-        if (j == 0){  // pierwszy wiersz
-          if (i != 0 && i != XMAX-1){
-            if (temp[i-1][j])
-              neighbors++;
-            if (temp[i-1][j+1])
-              neighbors++;
-            if (temp[i][j+1])
-              neighbors++;
-            if (temp[i+1][j+1])
-              neighbors++;
-            if (temp[i+1][j])
-              neighbors++;
-          }
-          if (i == 0) {  // 1. krawedz  [0][0]
-            if (temp[i+1][j] && temp[i+1][j+1] && temp[i][j+1])
-              neighbors += 3;
-          }
-          if (i == XMAX-1){  // 2. krawedz  [XMAX][0]
-            if (temp[i-1][j] && temp[i-1][j+1] && temp[i][j+1])
-              neighbors += 3;
-          }
+      neighbors = 0;
+      // pierwszy wiersz
+      if (j == 0){
+        if (i != 0 && i != XMAX-1){
+          if (temp[i-1][j])
+            neighbors++;
+          if (temp[i-1][j+1])
+            neighbors++;
+          if (temp[i][j+1])
+            neighbors++;
+          if (temp[i+1][j+1])
+            neighbors++;
+          if (temp[i+1][j])
+            neighbors++;
         }
-
-        if (j > 0 && j < YMAX-1){  // wiersze wewnetrzne
-          if (i == 0){  // pierwsza kolumna
-            if (temp[i][j-1])
-              neighbors++;
-            if (temp[i+1][j-1])
-              neighbors++;
-            if (temp[i+1][j])
-              neighbors++;
-            if (temp[i+1][j+1])
-              neighbors++;
-            if (temp[i][j+1])
-              neighbors++;
-          }
-          if (i > 0 && i < XMAX-1){  // komorki wewnetrzne
-            if (temp[i-1][j-1])
-              neighbors++;
-            if (temp[i][j-1])
-              neighbors++;
-            if (temp[i+1][j-1])
-              neighbors++;
-            if (temp[i+1][j])
-              neighbors++;
-            if (temp[i+1][j+1])
-              neighbors++;
-            if (temp[i][j+1])
-              neighbors++;
-            if (temp[i-1][j+1])
-              neighbors++;
-            if (temp[i-1][j])
-              neighbors++;
-          }
-
-          if (i == XMAX-1){  // ostatnia kolumna
-            if (temp[i][j-1])
-              neighbors++;
-            if (temp[i-1][j-1])
-              neighbors++;
-            if (temp[i-1][j])
-              neighbors++;
-            if (temp[i-1][j+1])
-              neighbors++;
-            if (temp[i][j+1])
-              neighbors++;
-          }
+        // 1. krawedz   [0][0]
+        if (i == 0) {
+          if (temp[i+1][j])
+            neighbors++;
+          if (temp[i+1][j+1])
+            neighbors++;
+          if (temp[i][j+1])
+            neighbors++;
         }
-
-        if (j == YMAX-1){  // ostatni wiersz
-          if (i != 0 && i != XMAX-1){
-            if (temp[i-1][j])
-              neighbors++;
-            if (temp[i-1][j-1])
-              neighbors++;
-            if (temp[i][j-1])
-              neighbors++;
-            if (temp[i+1][j-1])
-              neighbors++;
-            if (temp[i+1][j])
-              neighbors++;
-          }
-          if (i == 0){  // 3. krawedz   [0][YMAX]
-            if (temp[i][j-1] && temp[i+1][j-1] && temp[i+1][j])
-              neighbors += 3;
-          }
-          if (i == XMAX-1){ // 4. krawedz    [XMAX][YMAX]
-            if (temp[i-1][j] && temp[i-1][j-1] && temp[i][j-1])
-              neighbors += 3;
-          }
+        // 2. krawedz   [XMAX][0]
+        if (i == XMAX-1){
+          if (temp[i-1][j])
+            neighbors++;
+          if (temp[i-1][j+1])
+            neighbors++;
+          if (temp[i][j+1])
+            neighbors++;
         }
       }
-      if (neighbors >=3)
+      // wiersze wewnetrzne
+      if (j > 0 && j < YMAX-1){
+        // pierwsza kolumna
+        if (i == 0){
+          if (temp[i][j-1])
+            neighbors++;
+          if (temp[i+1][j-1])
+            neighbors++;
+          if (temp[i+1][j])
+            neighbors++;
+          if (temp[i+1][j+1])
+            neighbors++;
+          if (temp[i][j+1])
+            neighbors++;
+        }
+        // komorki wewnetrzne
+        if (i > 0 && i < XMAX-1){
+          if (temp[i-1][j-1])
+            neighbors++;
+          if (temp[i][j-1])
+            neighbors++;
+          if (temp[i+1][j-1])
+            neighbors++;
+          if (temp[i+1][j])
+            neighbors++;
+          if (temp[i+1][j+1])
+            neighbors++;
+          if (temp[i][j+1])
+            neighbors++;
+          if (temp[i-1][j+1])
+            neighbors++;
+          if (temp[i-1][j])
+            neighbors++;
+        }
+        // ostatnia kolumna
+        if (i == XMAX-1){
+          if (temp[i][j-1])
+            neighbors++;
+          if (temp[i-1][j-1])
+            neighbors++;
+          if (temp[i-1][j])
+            neighbors++;
+          if (temp[i-1][j+1])
+            neighbors++;
+          if (temp[i][j+1])
+            neighbors++;
+        }
+      }
+      // ostatni wiersz
+      if (j == YMAX-1){
+        if (i != 0 && i != XMAX-1){
+          if (temp[i-1][j])
+            neighbors++;
+          if (temp[i-1][j-1])
+            neighbors++;
+          if (temp[i][j-1])
+            neighbors++;
+          if (temp[i+1][j-1])
+            neighbors++;
+          if (temp[i+1][j])
+            neighbors++;
+        }
+        // 3. krawedz   [0][YMAX]
+        if (i == 0){
+          if (temp[i][j-1])
+            neighbors++;
+          if (temp[i+1][j-1])
+            neighbors++;
+          if (temp[i+1][j])
+            neighbors++;
+        }
+        // 4. krawedz   [XMAX][YMAX]
+        if (i == XMAX-1){
+          if (temp[i-1][j])
+            neighbors++;
+          if (temp[i-1][j-1])
+            neighbors++;
+          if (temp[i][j-1])
+            neighbors++;
+        }
+      }
+      // martwa komórka ma co najmniej 3 sąsiadów - staje się żywa
+      if (neighbors >= 3 && temp[i][j] == false)
         tab[i][j] = true;
-
-      if (temp[i][j] == true){
-
-
-      }
-
+      // żywa komórka nie ma 2 lub 3 sąsiadów - staje sie martwa
+      if (neighbors < 2 && neighbors > 3 && temp[i][j] == true)
+        tab[i][j] = false;
     }
   }
-
 }
