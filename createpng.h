a5db63a6a282429a4a2000af6487e64ab9be97ac
ksoloducha <58275941+ksoloducha@users.noreply.github.com>
2020-03-17 19:46:07 +0100

    Literówka

--- createpng.h
+++ createpng.h
@@ -2,7 +2,7 @@
 #define _CREATEPNG_H_
 
 /* funkcja tworząca plik png z przygotowanych wcześniej danych */
-void write_png_file(bool *array, char* file_name);
+void write_png_file(bool **array, char* file_name);
 /* funkcja czytająca dane z tabeli bool przekazywanej jako argument
  * oraz przygotowująca z nich dane do stworzenia pliku png */
 void process_arr(bool **array);
