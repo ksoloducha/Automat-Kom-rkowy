2623f46e78cc16ee7616587a8acd8e713e898ae3
kamx9 <61712110+kamx9@users.noreply.github.com>
2020-03-17 10:22:56 +0100

    zmiana kodu, krótszy - większa przejrzystość

--- generation.c
+++ generation.c
@@ -2,141 +2,38 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "generation.h"
 estern int XMAX, YMAX;
 
-void move (bool **tab){
+void move (bool **array){
   // tymczasowa tablica ze stanami początkowymi
   bool temp[XMAX][YMAX];
   for (int i = 0; i < XMAX; i++ ){
     for (int j = 0; j < YMAX; j++ ){
-      temp[i][j] = tab[i][j];
+      temp[i][j] = array[i][j];
     }
   }
 
   int neighbors;
   for (int i = 0; i < XMAX; i++ ){
     for (int j = 0; j < YMAX; j++ ){
+      
       neighbors = 0;
-      // pierwszy wiersz
-      if (j == 0){
-        if (i != 0 && i != XMAX-1){
-          if (temp[i-1][j])
-            neighbors++;
-          if (temp[i-1][j+1])
-            neighbors++;
-          if (temp[i][j+1])
-            neighbors++;
-          if (temp[i+1][j+1])
-            neighbors++;
-          if (temp[i+1][j])
-            neighbors++;
-        }
-        // 1. krawedz   [0][0]
-        if (i == 0) {
-          if (temp[i+1][j])
-            neighbors++;
-          if (temp[i+1][j+1])
-            neighbors++;
-          if (temp[i][j+1])
-            neighbors++;
-        }
-        // 2. krawedz   [XMAX][0]
-        if (i == XMAX-1){
-          if (temp[i-1][j])
-            neighbors++;
-          if (temp[i-1][j+1])
-            neighbors++;
-          if (temp[i][j+1])
-            neighbors++;
-        }
-      }
-      // wiersze wewnetrzne
-      if (j > 0 && j < YMAX-1){
-        // pierwsza kolumna
-        if (i == 0){
-          if (temp[i][j-1])
-            neighbors++;
-          if (temp[i+1][j-1])
-            neighbors++;
-          if (temp[i+1][j])
-            neighbors++;
-          if (temp[i+1][j+1])
-            neighbors++;
-          if (temp[i][j+1])
-            neighbors++;
-        }
-        // komorki wewnetrzne
-        if (i > 0 && i < XMAX-1){
-          if (temp[i-1][j-1])
-            neighbors++;
-          if (temp[i][j-1])
-            neighbors++;
-          if (temp[i+1][j-1])
-            neighbors++;
-          if (temp[i+1][j])
-            neighbors++;
-          if (temp[i+1][j+1])
-            neighbors++;
-          if (temp[i][j+1])
-            neighbors++;
-          if (temp[i-1][j+1])
-            neighbors++;
-          if (temp[i-1][j])
-            neighbors++;
-        }
-        // ostatnia kolumna
-        if (i == XMAX-1){
-          if (temp[i][j-1])
-            neighbors++;
-          if (temp[i-1][j-1])
-            neighbors++;
-          if (temp[i-1][j])
-            neighbors++;
-          if (temp[i-1][j+1])
-            neighbors++;
-          if (temp[i][j+1])
-            neighbors++;
-        }
-      }
-      // ostatni wiersz
-      if (j == YMAX-1){
-        if (i != 0 && i != XMAX-1){
-          if (temp[i-1][j])
-            neighbors++;
-          if (temp[i-1][j-1])
-            neighbors++;
-          if (temp[i][j-1])
-            neighbors++;
-          if (temp[i+1][j-1])
-            neighbors++;
-          if (temp[i+1][j])
-            neighbors++;
-        }
-        // 3. krawedz   [0][YMAX]
-        if (i == 0){
-          if (temp[i][j-1])
-            neighbors++;
-          if (temp[i+1][j-1])
-            neighbors++;
-          if (temp[i+1][j])
-            neighbors++;
-        }
-        // 4. krawedz   [XMAX][YMAX]
-        if (i == XMAX-1){
-          if (temp[i-1][j])
-            neighbors++;
-          if (temp[i-1][j-1])
-            neighbors++;
-          if (temp[i][j-1])
-            neighbors++;
-        }
-      }
-      // martwa komórka ma co najmniej 3 sąsiadów - staje się żywa
-      if (neighbors >= 3 && temp[i][j] == false)
-        tab[i][j] = true;
-      // żywa komórka nie ma 2 lub 3 sąsiadów - staje sie martwa
-      if ((neighbors < 2 || neighbors > 3) && temp[i][j] == true)
-        tab[i][j] = false;
+      
+      for(int x = i - 1; x <= i + 1; x++)
+				for(int y = j - 1; y <= j + 1; y++){
+
+					if(x < 0 || x == XMAX || y < 0 || y == YMAX)
+						continue;
+
+					else if(tmp[x][y])
+						neighbors++;
+				}
+			if(!tmp[i][j] && neighbors >= 3)
+				array[i][j] = true;
+			else if(tmp[i][j] && neighbors != 3 && neighbors != 4)
+				array[i][j] = false;
+      
     }
   }
 }
