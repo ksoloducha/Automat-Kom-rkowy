d8dee14b2fe5060ad4c2b8888a37ebbd3c9e4f0b
kamx9 <61712110+kamx9@users.noreply.github.com>
2020-03-17 10:26:39 +0100

    małe poprawki

--- generation.h
+++ generation.h
@@ -2,13 +2,13 @@
 #define _GENERATION_H_
 
 /* program ma na celu modyfikować tablice typu bool w taki sposób, że:
- * martwe komórki posiadajace co najmniej 3 żywych sasiadów stają się żywe,
+ * martwe komórki posiadajace dokładnie 3 żywych sasiadów stają się żywe,
  * żywe komórki są dalej żywe tylko wtedy gdy mają 2 lub 3 sąsiadów,
  * w innych wypadkach stają się martwe  */
 
 /* funkcja przyjmuje wskaźnik na oryginalną tablice i tworzy tymczasowa w celu
  * zapamiętania początkowego układu komórek, modyfikuje oryginalną tablice
  * według reguły powyżej  */
-void move (bool **tab);
+void move (bool **array);
 
 #endif
